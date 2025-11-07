f4e01519778f874488a068b0c2f6b52d9884fd0e
bafokengBaffy <baffkay20@gmail.com>
2025-11-07 04:11:58 +0200

    Remove type: module from package.json to fix import issues with Create React App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from './App'; // Remove .jsx extension
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
